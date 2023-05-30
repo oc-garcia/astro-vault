2f54ee3afb373c874e0e06da7b62086a4e665502
oc-garcia <octaviogarcia1988@gmail.com>
2023-05-30 16:26:46 -0300

    api request implemented

--- src/pages/MarsRovers.tsx
+++ src/pages/MarsRovers.tsx
@@ -1,19 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { fetchMarsRover } from "../services/fetchMarsRover";
 
 export default function MarsRovers() {
   const [data, setData] = useState();
-  
+
+  const handleData = async (rover: string) => {
+    const roverData = await fetchMarsRover(rover);
+    setData(roverData);
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <section className="bg-slate-700 p-4 text-gray-50 flex flex-col items-center justify-center gap-1">
       <h2 className="text-left self-start	font-bold mt-2">Pick a rover:</h2>
       <ul className="self-start">
-        <li className="hover:bg-slate-900 bg-slate-800 ml-2 mt-2 list-none cursor-pointer border-solid border-2 border-cyan-700 hover:border-violet-600 rounded p-4 font-bold transition-all">
+        <li
+          className="hover:bg-slate-900 bg-slate-800 ml-2 mt-2 list-none cursor-pointer border-solid border-2 border-cyan-700 hover:border-violet-600 rounded p-4 font-bold transition-all"
+          onClick={() => handleData("curiosity")}>
           Curiosity
         </li>
-        <li className="hover:bg-slate-900 bg-slate-800 ml-2 mt-2 list-none cursor-pointer border-solid border-2 border-cyan-700 hover:border-violet-600 rounded p-4 font-bold transition-all">
+        <li
+          className="hover:bg-slate-900 bg-slate-800 ml-2 mt-2 list-none cursor-pointer border-solid border-2 border-cyan-700 hover:border-violet-600 rounded p-4 font-bold transition-all"
+          onClick={() => handleData("opportunity")}>
           Opportunity
         </li>
-        <li className="hover:bg-slate-900 bg-slate-800 ml-2 mt-2 list-none cursor-pointer border-solid border-2 border-cyan-700 hover:border-violet-600 rounded p-4 font-bold transition-all">
+        <li
+          className="hover:bg-slate-900 bg-slate-800 ml-2 mt-2 list-none cursor-pointer border-solid border-2 border-cyan-700 hover:border-violet-600 rounded p-4 font-bold transition-all"
+          onClick={() => handleData("spirit")}>
           Spirit
         </li>
       </ul>

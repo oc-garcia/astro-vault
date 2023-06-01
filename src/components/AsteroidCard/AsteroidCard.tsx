d3b78ac9f3425cb6f5fd2d932171e105eb068372
oc-garcia <octaviogarcia1988@gmail.com>
2023-06-01 18:01:51 -0300

    debugging

--- src/components/AsteroidCard/AsteroidCard.tsx
+++ src/components/AsteroidCard/AsteroidCard.tsx
@@ -1,8 +1,32 @@
 import { handleDate } from "../../services/handleDate";
 import { handleNumberFormat } from "../../services/handleNumberFormat";
-import { IAsteroid } from "../../types/IAsteroid";
 
-export default function AsteroidCard({ asteroid }: IAsteroid) {
+interface IAsteroidCard {
+  name: string;
+  absolute_magnitude_h: number;
+  is_potentially_hazardous_asteroid: boolean;
+  is_sentry_object: boolean;
+  estimated_diameter: {
+    kilometers: {
+      estimated_diameter_max: number;
+      estimated_diameter_min: number;
+    };
+  };
+  close_approach_data: [
+    {
+      close_approach_date: string;
+      miss_distance: {
+        kilometers: number;
+      };
+      orbiting_body: string;
+      relative_velocity: {
+        kilometers_per_hour: number;
+      };
+    }
+  ];
+}
+
+export default function AsteroidCard( asteroid : IAsteroidCard) {
   return (
     <li className="hover:scale-110 hover:bg-slate-900 bg-slate-800 list-none border-solid border-2 border-blue-700 rounded p-4 transition-all ">
       <h3>Name: {asteroid?.name}</h3>
--- src/pages/Asteroids.tsx
+++ src/pages/Asteroids.tsx
@@ -4,32 +4,34 @@ import AsteroidCard from "../components/AsteroidCard/AsteroidCard";
 import { IAsteroid } from "../types/IAsteroid";
 import Loader from "../components/Loader/Loader";
 
-const defaultValue: IAsteroid[] = {
-  asteroid: {
-    name: "string",
-    absolute_magnitude_h: 0,
-    is_potentially_hazardous_asteroid: false,
-    is_sentry_object: false,
-    estimated_diameter: {
-      kilometers: {
-        estimated_diameter_max: 0,
-        estimated_diameter_min: 0,
+const defaultValue: IAsteroid[] = [
+  {
+    asteroid: {
+      name: "",
+      absolute_magnitude_h: 0,
+      is_potentially_hazardous_asteroid: false,
+      is_sentry_object: false,
+      estimated_diameter: {
+        kilometers: {
+          estimated_diameter_max: 0,
+          estimated_diameter_min: 0,
+        },
       },
+      close_approach_data: [
+        {
+          close_approach_date: "",
+          miss_distance: {
+            kilometers: 0,
+          },
+          orbiting_body: "",
+          relative_velocity: {
+            kilometers_per_hour: 0,
+          },
+        },
+      ],
     },
-    close_approach_data: [
-      {
-        close_approach_date: "",
-        miss_distance: {
-          kilometers: 0,
-        },
-        orbiting_body: "",
-        relative_velocity: {
-          kilometers_per_hour: 0,
-        },
-      },
-    ],
   },
-};
+];
 
 export default function Asteroids() {
   const [data, setData] = useState(defaultValue);

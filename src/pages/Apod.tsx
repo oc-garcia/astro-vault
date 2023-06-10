54d59563cf0516fde0ae500f5134732114164bc9
oc-garcia <octaviogarcia1988@gmail.com>
2023-06-10 15:58:25 -0300

    Removed console.log

--- src/pages/Apod.tsx
+++ src/pages/Apod.tsx
@@ -19,8 +19,6 @@ export default function Apod() {
     handleData();
   }, []);
 
-  console.log(data);
-
   return (
     <section className="bg-slate-700 p-4 text-gray-50 flex flex-col items-center justify-center gap-1 container">
       {data != undefined && (
